4da9d4b53082248f464ed04ea79dceafc0794d4e
Aaron Mildenstein <aaron@mildensteins.com>
2015-11-05 15:22:09 -0800

    Initial commit

---
+++ lib/logstash/filters/de_dot.rb
@@ -0,0 +1,58 @@
+# encoding: utf-8
+require "logstash/filters/base"
+require "logstash/namespace"
+
+# This filter _appears_ to rename fields by replacing `.` characters with a different
+# separator.  In reality, it's a somewhat expensive filter that has to copy the
+# source field contents to a new destination field (whose name no longer contains
+# dots), and then remove the corresponding source field.
+#
+# It should only be used if no other options are available.
+class LogStash::Filters::De_dot < LogStash::Filters::Base
+
+  config_name "de_dot"
+
+  # Replace dots with this value.
+  config :separator, :validate => :string, :default => "_"
+
+  # If `nested` is _true_, then create sub-fields instead of replacing dots with
+  # a different separator.
+  config :nested, :validate => :boolean, :default => false
+
+  # The `fields` array should contain a list of known fields to act on.
+  # If undefined, all top-level fields will be checked.  Sub-fields must be
+  # manually specified in the array.  For example: `["field.suffix","[foo][bar.suffix]"]`
+  # will result in "field_suffix" and nested or sub field ["foo"]["bar_suffix"]
+  #
+  # WARNING: This is an expensive operation.
+  #
+  config :fields, :validate => :array
+
+
+  public
+  def register
+    # Add instance variables, if any
+  end # def register
+
+  private
+  def rename_field(event, fieldref)
+    @logger.debug? and @logger.debug("preprocess", :event => event.to_hash.to_s)
+    @logger.debug? and @logger.debug("source field reference", :fieldref => fieldref)
+    newref = fieldref.gsub('.', @separator)
+    @logger.debug? and @logger.debug("replacement field reference", :newref => newref)
+    event[newref] = event[fieldref]
+    @logger.debug? and @logger.debug("event with both new and old field references", :event => event.to_hash.to_s)
+    event.remove(fieldref)
+    @logger.debug? and @logger.debug("postprocess", :event => event.to_hash.to_s)
+  end
+
+  public
+  def filter(event)
+    @separator = '][' if @nested
+    @logger.debug? and @logger.debug("Replace dots with separator", :separator => @separator)
+    @fields = event.to_hash.keys if @fields.nil?
+    @logger.debug? and @logger.debug("Act on these fields", :fields => @fields)
+    @fields.each { |ref| rename_field(event, ref) if !(ref =~ /\./).nil? }
+    filter_matched(event)
+  end # def filter
+end # class LogStash::Filters::De_dot
---
+++ spec/filters/de_dot_spec.rb
@@ -0,0 +1,182 @@
+# encoding: utf-8
+require 'spec_helper'
+require "logstash/filters/de_dot"
+
+describe LogStash::Filters::De_dot do
+  let(:config) { { } }
+  subject      { LogStash::Filters::De_dot.new(config) }
+
+  let(:attrs) { { } }
+  let(:event) { LogStash::Event.new(attrs) }
+
+  before(:each) do
+    subject.register
+  end
+
+  describe "Single field" do
+    let(:attrs) { { "foo.bar" => "pass" } }
+
+    it "should replace a dot with an underscore" do
+      subject.filter(event)
+      expect(event.to_hash.keys).not_to include('foo.bar')
+      expect(event['foo_bar']).to eq('pass')
+    end
+  end
+
+  describe "Single field with alternate separator" do
+    let(:config) { { "separator" => "___" } }
+    let(:attrs) { { "foo.bar" => "pass" } }
+
+    it "should replace a dot with an underscore" do
+      subject.filter(event)
+      expect(event.to_hash.keys).not_to include('foo.bar')
+      expect(event['foo___bar']).to eq('pass')
+    end
+  end
+
+  describe "Multiple fields" do
+    let(:attrs) { { "acme.roller.skates" => "coyote", "nodot" => "nochange" } }
+
+    it "should replace all dots with underscores" do
+      subject.filter(event)
+      expect(event.to_hash.keys).not_to include('acme.roller.skates')
+      expect(event['acme_roller_skates']).to eq('coyote')
+    end
+
+    it "should not change a field without dots" do
+      subject.filter(event)
+      expect(event.to_hash.keys).to include('nodot')
+      expect(event['nodot']).to eq('nochange')
+    end
+  end
+
+  describe "Multiple fields with underscores already" do
+    let(:attrs) { { "acme_roller.skates" => "coyote", "no_dot" => "nochange" } }
+
+    it "should replace all dots with underscores" do
+      subject.filter(event)
+      expect(event.to_hash.keys).not_to include('acme_roller.skates')
+      expect(event['acme_roller_skates']).to eq('coyote')
+    end
+
+    it "should not change a field without dots" do
+      subject.filter(event)
+      expect(event.to_hash.keys).to include('no_dot')
+      expect(event['no_dot']).to eq('nochange')
+    end
+  end
+
+  describe "Nested fields" do
+    let(:config) { { "nested" => true } }
+    let(:attrs) { { "acme.roller.skates" => "coyote", "nodot" => "nochange" } }
+
+    it "should convert dotted fields to sub-fields" do
+      subject.filter(event)
+      expect(event.to_hash.keys).not_to include('acme.roller.skates')
+      expect(event['[acme][roller][skates]']).to eq('coyote')
+    end
+
+    it "should not change a field without dots" do
+      subject.filter(event)
+      expect(event.to_hash.keys).to include('nodot')
+      expect(event['nodot']).to eq('nochange')
+    end
+  end
+
+  describe "Specific nested field" do
+    let(:config) { { "fields" => [ "[acme][roller.skates]" ] } }
+    let(:attrs) { { "acme" => { "roller.skates" => "coyote" }, "foo.bar" => "nochange" } }
+
+    it "should replace all dots with underscores within specified fields" do
+      subject.filter(event)
+      expect(event['acme']).not_to include('roller.skates')
+      expect(event['[acme][roller_skates]']).to eq('coyote')
+    end
+
+    it "should not change a field not listed, even with dots" do
+      subject.filter(event)
+      expect(event.to_hash.keys).to include('foo.bar')
+      expect(event['foo.bar']).to eq('nochange')
+    end
+  end
+
+  describe "Multiple specific nested fields" do
+    let(:config) {
+      {
+        "nested" => true,
+        "fields" => [ "[acme][roller.skates]", "foo.bar", "[a.b][c.d][e.f]" ]
+      }
+    }
+    let(:attrs) {
+      {
+        "acme" => { "roller.skates" => "coyote" },
+        "foo.bar" => "nochange",
+        "a.b" => { "c.d" => { "e.f" => "finally"} }
+      }
+    }
+
+    it "should replace all dots with underscores within specified fields" do
+      subject.filter(event)
+      expect(event['acme']).not_to include('roller.skates')
+      expect(event['[acme][roller][skates]']).to eq('coyote')
+      expect(event.to_hash.keys).not_to include('foo.bar')
+      expect(event['[foo][bar]']).to eq('nochange')
+      expect(event.to_hash.keys).not_to include('a.a')
+      expect(event['[a][b][c][d][e][f]']).to eq('finally')
+    end
+  end
+
+  describe "Specific nested fields with underscores already" do
+    let(:config) {
+      {
+        "fields" => [ "[acme][super.roller_skates]", "[field_with][no_dot]" ]
+      }
+    }
+    let(:attrs) {
+      {
+        "acme" => { "super.roller_skates" => "coyote" },
+        "field_with" => { "no_dot" => "nochange" }
+      }
+    }
+
+    it "should replace all dots with underscores" do
+      subject.filter(event)
+      expect(event["acme"]).not_to include('super.roller_skates')
+      expect(event['[acme][super_roller_skates']).to eq('coyote')
+    end
+
+    it "should not change a field without dots" do
+      subject.filter(event)
+      expect(event.to_hash.keys).to include('field_with')
+      expect(event['[field_with][no_dot]']).to eq('nochange')
+    end
+  end
+
+  describe "Further nesting specific nested fields with underscores already" do
+    let(:config) {
+      {
+        "nested" => true,
+        "fields" => [ "[acme][super.roller_skates]", "[field_with][no_dot]" ]
+      }
+    }
+    let(:attrs) {
+      {
+        "acme" => { "super.roller_skates" => "coyote" },
+        "field_with" => { "no_dot" => "nochange" }
+      }
+    }
+
+    it "should replace all dots with underscores" do
+      subject.filter(event)
+      expect(event["acme"]).not_to include('super.roller_skates')
+      expect(event['[acme][super][roller_skates']).to eq('coyote')
+    end
+
+    it "should not change a field without dots" do
+      subject.filter(event)
+      expect(event.to_hash.keys).to include('field_with')
+      expect(event['[field_with][no_dot]']).to eq('nochange')
+    end
+  end
+
+end
